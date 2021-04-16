7fc2e7a807b346bed472351aaf80585b68031e5e
shashiradw <shashiradw7@gmail.com>
2021-04-16 08:14:07 +0530

    Added complete Icon

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,12 @@ class _QuizAppState extends State<QuizApp> {
           backgroundColor: Colors.white30,
         ),
         backgroundColor: Colors.black38,
-
+        
         body: Center(
           child: Column(
             children: [
-                 Expanded(
+              Expanded(
+                   flex: 4,
                   child: Container(
                     margin: EdgeInsets.only(left: 20.0, right: 20.0),
                     alignment: Alignment.center,
@@ -37,36 +38,37 @@ class _QuizAppState extends State<QuizApp> {
                     ),
                   ),
                 ),
-              //True Btn
-             TextButton(
-                 onPressed: (){
-                 },
-                 child: Container(
-                   margin: EdgeInsets.only(bottom: 20.0),
-                   alignment: Alignment.center,
-                   color: Colors.green,
-                   width: 250.0,
-                   height: 50.0,
-                   child: Text('True',
-                     style: TextStyle(
-                       color: Colors.white,
-                       fontSize: 20.0,
+                //True Btn
+              Expanded(
+                 flex: 1,
+                 child: TextButton(
+                     onPressed: (){
+                     },
+                     child: Container(
+                       margin: EdgeInsets.only(bottom: 40.0),
+                       alignment: Alignment.center,
+                       color: Colors.green,
+                       width: 250.0,
+                       child: Text('True',
+                         style: TextStyle(
+                           color: Colors.white,
+                           fontSize: 20.0,
+                         ),
+                       ),
                      ),
-                   ),
                  ),
-             ),
-
+               ),
               //False btn
-              Center(
+              Expanded(
+                flex: 1,
                 child: TextButton(
                   onPressed: (){
                   },
                   child: Container(
-                    margin: EdgeInsets.only(bottom: 50.0),
+                    margin: EdgeInsets.only(bottom: 40.0),
                     alignment: Alignment.center,
                     color: Colors.red,
                     width: 250.0,
-                    height: 50.0,
                     child: Text('False',
                       style: TextStyle(
                         color: Colors.white,
@@ -75,7 +77,37 @@ class _QuizAppState extends State<QuizApp> {
                     ),
                   ),
                 ),
-              )
+              ),
+
+              Row(
+                children: [
+                  Icon(
+                    Icons.check,
+                    color:Colors.green,
+                  ),
+                  Icon(
+                    Icons.close,
+                    color:Colors.red,
+                  ),
+                  Icon(
+                    Icons.close,
+                    color:Colors.red,
+                  ),
+                  Icon(
+                    Icons.close,
+                    color:Colors.red,
+                  ),
+                  Icon(
+                    Icons.close,
+                    color:Colors.red,
+                  ),
+                  Icon(
+                    Icons.close,
+                    color:Colors.red,
+                  ),
+                ],
+              ),
+
 
             ],
           ),
